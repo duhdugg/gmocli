6013c311714116d05d9ba5f715c333e798ab6287
Doug Elkin <doug@dougelkin.com>
2022-03-15 17:04:11 -0400

    ⬆️ upgrade deps

--- scripts/update-emoji-data.sh
+++ scripts/update-emoji-data.sh
@@ -3,6 +3,6 @@
 cd dev
 mkdir -p emoji-data-dist
 cd emoji-data-dist
-wget https://github.com/Mange/emoji-data/releases/download/v2.3/prebuilt-data-v2.3.tar.bz2
-tar -xf prebuilt-data-v2.3.tar.bz2
+wget https://github.com/Mange/emoji-data/releases/download/v2.4/prebuilt-data-v2.4.tar.bz2
+tar -xf prebuilt-data-v2.4.tar.bz2
 cat data/all_emojis.json | jq -c | gzip > ../../data/emojis.json.gz
