c676d9f9e6924b6d9d404826eb7dbece33883940
Doug Elkin <doug@dougelkin.com>
2025-04-03 02:31:41 -0400

    ⬆️ upgrade deps
    now supporting Emoji 16.0 🫩

--- scripts/update-emoji-data.sh
+++ scripts/update-emoji-data.sh
@@ -5,19 +5,19 @@ set -euxo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR/..
 
-EMOJI_DATA_VERSION="2.6"
-GITMOJI_VERSION="3.14.0"
-EMOJI_URL="https://github.com/Mange/emoji-data/releases/download/v$EMOJI_DATA_VERSION/prebuild-data-v$EMOJI_DATA_VERSION.tar.bz2"
+EMOJI_DATA_VERSION="2.7-prerelease-pr-31"
+GITMOJI_VERSION="3.15.0"
+EMOJI_URL="https://github.com/duhdugg/emoji-data/releases/download/v$EMOJI_DATA_VERSION/prebuilt-data-v$EMOJI_DATA_VERSION.tar.bz2"
 GITMOJI_URL="https://github.com/carloscuesta/gitmoji/archive/refs/tags/v$GITMOJI_VERSION.tar.gz"
 
 mkdir -p data/emoji data/gitmoji dev/emoji-data-dist dev/gitmoji
 
 cd dev/emoji-data-dist
-curl -L "$EMOJI_URL" > prebuilt-data.tar.bz2
+curl -L "$EMOJI_URL" > "prebuilt-data.tar.bz2"
 tar -xvaf prebuilt-data.tar.bz2
-cat data/all_emojis.json | jq -c  > ../../data/emoji/emoji.json
+cat data/all_emojis.json | jq -c  > "../../data/emoji/emoji.json"
 
 cd ../gitmoji
-curl -L "$GITMOJI_URL" > gitmoji.tar.gz
+curl -L "$GITMOJI_URL" > "gitmoji.tar.gz"
 tar -xf gitmoji.tar.gz
-cat "gitmoji-$GITMOJI_VERSION/packages/gitmojis/src/gitmojis.json" | jq -c > ../../data/gitmoji/gitmoji.json
+cat "gitmoji-$GITMOJI_VERSION/packages/gitmojis/src/gitmojis.json" | jq -c > "../../data/gitmoji/gitmoji.json"
